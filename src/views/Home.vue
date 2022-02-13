1d163f1cb32a7a87ac840416f2b51148b71d7925
Eric Wang <beatofangel@gmx.com>
2022-02-14 00:34:12 +0800

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,31 @@
 <template>
-  <div class="home">
-    <span>{{$t('label.home')}}</span>
-  </div>
+  <v-container class="mt-6">
+    <v-row>
+      <v-col>
+        <v-card height="500">
+          <v-card-title>
+            <span>{{ $t("label.home") }}</span>
+          </v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col class="text-center">
+                <v-btn
+                  color="blue-grey"
+                  href="https://github.com/beatofangel/SwiftConnector/raw/master/SwiftConnector/publish/setup.exe"
+                  dark
+                  >Download Swift Connector</v-btn
+                >
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-}
+  name: "Home",
+};
 </script>
